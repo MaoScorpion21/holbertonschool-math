640fe26dc327473df65a8add1cb5979d310fb537
MaoScorpion21 <mao7021@hotmail.com>
2022-03-22 22:19:51 -0500

    0-display.c

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * display_complex_number - Function that displays the
+ * complex numbers, followed by a new line.
+ * @c: Complex number
+ */
+
+void display_complex_number(complex c)
+{
+if (c.re == 0 && c.im == 0)
+	printf("%d", 0);
+	else
+	{
+		if (c.re == 0 && c.im != 0)
+			printf("%d", 0);
+		if (c.re != 0)
+			printf("%g", c.re);
+		if (c.im > 0)
+			printf(" + ");
+		else if (c.im < 0)
+			printf(" - ");
+		if (c.im > 0 && c.im != 1)
+			printf("%gi", c.im);
+		else if (c.im == 1)
+			printf("i");
+		else if (c.im < 0 && c.re != 0 && c.im != -1)
+			printf("%gi", c.im * -1);
+		else if (c.im < 0 && c.re != 0 && c.im == -1)
+			printf("i");
+		else if (c.im < 0 && c.re == 0 && c.im != -1)
+			printf("%gi", c.im);
+		else if (c.im < 0 && c.re == 0 && c.im == -1)
+			printf("i");
+	}
+	printf("\n");
+}
